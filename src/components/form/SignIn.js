e56f257733c70e0c8bf6e63f8f8babcab2b2154e
Mostafa Ezzat <psmostafa@gmail.com>
2022-07-08 21:43:28 +0200

    add ( add ticket button to the ticket page)

--- src/components/form/SignIn.js
+++ src/components/form/SignIn.js
@@ -13,90 +13,97 @@ import { auth } from "../../firebase";
 import { signInWithEmailAndPassword } from "firebase/auth";
 
 export default function Signin({ changeFrom }) {
-  const [email, setEmail] = useState("admin@gmail.com");
-  const [password, setPassword] = useState("passwordD2@");
-  const [error, setError] = useState({
-    displayName: "",
-    email: "",
-    password: "",
-  });
-  const [submitted, setSubmitted] = useState(true);
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState({
+        displayName: "",
+        email: "",
+        password: "",
+    });
+    const [submitted, setSubmitted] = useState(true);
 
-  useEffect(() => {
-    if (validateEmail(email) && validatePassword(password)) {
-      setSubmitted(false);
+    useEffect(() => {
+        if (validateEmail(email) && validatePassword(password)) {
+            setSubmitted(false);
+        }
+    }, [email, password]);
+
+    function emailHandle(e) {
+        setEmail(e.target.value);
+        if (validateEmail(e.target.value)) {
+            setError({ ...error, email: "" });
+        } else {
+            setError({ ...error, email: "Please Set Viable Email" });
+        }
     }
-  }, [email, password]);
 
-  function emailHandle(e) {
-    setEmail(e.target.value);
-    if (validateEmail(e.target.value)) {
-      setError({ ...error, email: "" });
-    } else {
-      setError({ ...error, email: "Please Set Viable Email" });
+    function passwordHandle(e) {
+        setPassword(e.target.value);
     }
-  }
 
-  function passwordHandle(e) {
-    setPassword(e.target.value);
-  }
+    async function handleSubmit(e) {
+        e.preventDefault();
+        setSubmitted(true);
 
-  async function handleSubmit(e) {
-    e.preventDefault();
-    setSubmitted(true);
-
-    try {
-      const signin = await signInWithEmailAndPassword(auth, email, password);
-    } catch (error) {
-      if (error.message == "Firebase: Error (auth/user-not-found).") {
-        setError({ ...error, email: "This Email Not Exist." });
-      } else if (error.message == "Firebase: Error (auth/wrong-password).") {
-        setError({
-          email: "",
-          password: "Please Make Sure You Typed Your Credential Right.",
-        });
-      }
+        try {
+            const signin = await signInWithEmailAndPassword(
+                auth,
+                email,
+                password
+            );
+        } catch (error) {
+            if (error.message == "Firebase: Error (auth/user-not-found).") {
+                setError({ ...error, email: "This Email Not Exist." });
+            } else if (
+                error.message == "Firebase: Error (auth/wrong-password)."
+            ) {
+                setError({
+                    email: "",
+                    password:
+                        "Please Make Sure You Typed Your Credential Right.",
+                });
+            }
+        }
     }
-  }
-  return (
-    <>
-      <h1 className="mb-8 w-96 border-b border-solid border-black/10 pb-2.5 text-center text-4xl font-bold">
-        {" "}
-        Sign Up{" "}
-      </h1>
-      <div className="w-96 space-y-4 rounded bg-white px-2.5 py-5 shadow-sm">
-        <form onSubmit={handleSubmit}>
-          <TextInput
-            label="Email:"
-            value={email}
-            handleChange={emailHandle}
-            error={error.email}
-          />
-          <PasswordInput
-            label="Password:"
-            value={password}
-            handleChange={passwordHandle}
-            error={error.password}
-          />
+    return (
+        <>
+            <h1 className="mb-8 w-96 border-b border-solid border-black/10 pb-2.5 text-center text-4xl font-bold">
+                {" "}
+                Sign Up{" "}
+            </h1>
+            <div className="w-96 space-y-4 rounded bg-white px-2.5 py-5 shadow-sm">
+                <form onSubmit={handleSubmit}>
+                    <TextInput
+                        label="Email:"
+                        value={email}
+                        handleChange={emailHandle}
+                        error={error.email}
+                    />
+                    <PasswordInput
+                        label="Password:"
+                        value={password}
+                        handleChange={passwordHandle}
+                        error={error.password}
+                    />
 
-          <button
-            type="submit"
-            className="bg-dodger-blue hover:bg-moody-blue disabled:bg-scorpion mt-5 w-full rounded py-2.5 text-sm font-semibold text-white"
-            disabled={submitted}
-          >
-            Sign Up
-          </button>
-        </form>
-      </div>
-      <div className="text-scorpion mt-2 font-medium">
-        Don&apos;t have an account yet?{" "}
-        <button
-          className="text-cod-gray hover:text-dodger-blue font-semibold outline-none transition-colors"
-          onClick={() => changeFrom("signup")}
-        >
-          SignUp Here
-        </button>
-      </div>
-    </>
-  );
+                    <button
+                        type="submit"
+                        className="mt-5 w-full rounded bg-dodger-blue py-2.5 text-sm font-semibold text-white hover:bg-moody-blue disabled:bg-scorpion"
+                        disabled={submitted}
+                    >
+                        Sign In
+                    </button>
+                </form>
+            </div>
+            <div className="mt-2 font-medium text-scorpion">
+                Don&apos;t have an account yet?{" "}
+                <button
+                    className="font-semibold text-cod-gray outline-none transition-colors hover:text-dodger-blue"
+                    onClick={() => changeFrom("signup")}
+                >
+                    SignUp Here
+                </button>
+            </div>
+        </>
+    );
 }
--- src/pages/tickets/index.js
+++ src/pages/tickets/index.js
@@ -22,94 +22,120 @@ import useGetAllProjects from "../../hooks/useGetAllProjects";
 import getManagerProjects from "../../util/getManagerProjects";
 
 const Tickets = () => {
-  const { tickets, loading, error } = useGetTickets();
-  const [filteredTickets, setFilteredTickets] = useState([]);
-  const {
-    projects,
-    error: projectsErrors,
-    loading: projectsLoading,
-  } = useGetAllProjects();
-  const [projectFilter, setProjectFilter] = useState({ title: "All" });
-  const [projectList, setProjectList] = useState([]);
-  const auth = useSelector((state) => state.auth);
+    const { tickets, loading, error } = useGetTickets();
+    const [filteredTickets, setFilteredTickets] = useState([]);
+    const {
+        projects,
+        error: projectsErrors,
+        loading: projectsLoading,
+    } = useGetAllProjects();
+    const [projectFilter, setProjectFilter] = useState({ title: "All" });
+    const [projectList, setProjectList] = useState([]);
+    const auth = useSelector((state) => state.auth);
 
-  useEffect(() => {
-    const tempTickets = tickets.filter(
-      (ticket) =>
-        projectList.findIndex((project) => project.id == ticket.project.id) !==
-        -1
-    );
+    useEffect(() => {
+        const tempTickets = tickets.filter(
+            (ticket) =>
+                projectList.findIndex(
+                    (project) => project.id == ticket.project.id
+                ) !== -1
+        );
 
-    if (projectFilter.title == "All") {
-      setFilteredTickets(tempTickets);
-    } else {
-      setFilteredTickets(
-        tickets.filter((ticket) => ticket.project.id == projectFilter.id)
-      );
-    }
-  }, [projectFilter, tickets, projectList]);
+        if (projectFilter.title == "All") {
+            setFilteredTickets(tempTickets);
+        } else {
+            setFilteredTickets(
+                tickets.filter(
+                    (ticket) => ticket.project.id == projectFilter.id
+                )
+            );
+        }
+    }, [projectFilter, tickets, projectList]);
 
-  useEffect(() => {
-    const selectAllObject = { id: "all", title: "All" };
+    useEffect(() => {
+        const selectAllObject = { id: "all", title: "All" };
 
-    if (!projectsLoading) {
-      if (auth.user.role == "admin") {
-        setProjectList([selectAllObject, ...projects]);
-      } else {
-        setProjectList([
-          selectAllObject,
-          ...getManagerProjects(auth, projects),
-        ]);
-      }
-    }
-  }, [projects]);
+        if (!projectsLoading) {
+            if (auth.user.role == "admin") {
+                setProjectList([selectAllObject, ...projects]);
+            } else {
+                setProjectList([
+                    selectAllObject,
+                    ...getManagerProjects(auth, projects),
+                ]);
+            }
+        }
+    }, [projects]);
 
-  if (loading || projectsLoading) return <Loading />;
-  if (!loading && error)
+    if (loading || projectsLoading) return <Loading />;
+    if (!loading && error)
+        return (
+            <Layout>
+                <div className="flex flex-col gap-4 px-3 md:flex-row md:justify-between">
+                    <div className="flex flex-col gap-1.5 md:flex-row md:items-center">
+                        <span className="font-medium text-scorpion">
+                            Filter By Project:
+                        </span>
+                        <DropdownInput
+                            list={projectList}
+                            setData={setProjectFilter}
+                            field={"title"}
+                            idField="id"
+                        />
+                    </div>
+
+                    <Link href="/tickets/new">
+                        <a className="">
+                            <div className="h-6 w-full rounded bg-white text-dodger-blue shadow-sm transition-all duration-200 hover:text-moody-blue hover:shadow-lg md:w-6">
+                                <Plus className="mx-auto h-6 w-6" />
+                            </div>
+                        </a>
+                    </Link>
+                </div>
+                <div className="mr-2.5 bg-white py-6 text-center font-bold text-scorpion shadow-sm">
+                    {error}
+                </div>
+            </Layout>
+        );
+
     return (
-      <Layout>
-        <div className="text-scorpion mr-2.5 bg-white py-6 text-center font-bold shadow-sm">
-          {error}
-        </div>
-      </Layout>
-    );
+        <Layout>
+            <div className="flex flex-col gap-4 px-3 md:flex-row md:justify-between">
+                <div className="flex flex-col gap-1.5 md:flex-row md:items-center">
+                    <span className="font-medium text-scorpion">
+                        Filter By Project:
+                    </span>
+                    <DropdownInput
+                        list={projectList}
+                        setData={setProjectFilter}
+                        field={"title"}
+                        idField="id"
+                    />
+                </div>
 
-  return (
-    <Layout>
-      <div className="flex flex-col gap-4 px-3 md:flex-row md:justify-between">
-        <div className="flex flex-col gap-1.5 md:flex-row md:items-center">
-          <span className="text-scorpion font-medium">Filter By Project:</span>
-          <DropdownInput
-            list={projectList}
-            setData={setProjectFilter}
-            field={"title"}
-            idField="id"
-          />
-        </div>
-
-        <Link href="/tickets/new">
-          <a className="">
-            <div className="text-dodger-blue hover:text-moody-blue h-6 w-full rounded bg-white shadow-sm transition-all duration-200 hover:shadow-lg md:w-6">
-              <Plus className="mx-auto h-6 w-6" />
+                <Link href="/tickets/new">
+                    <a className="">
+                        <div className="h-6 w-full rounded bg-white text-dodger-blue shadow-sm transition-all duration-200 hover:text-moody-blue hover:shadow-lg md:w-6">
+                            <Plus className="mx-auto h-6 w-6" />
+                        </div>
+                    </a>
+                </Link>
             </div>
-          </a>
-        </Link>
-      </div>
-      <ContentGrid>
-        {filteredTickets.length > 0 &&
-          filteredTickets.map((ticket) => (
-            <TicketCard
-              key={ticket.id}
-              id={ticket.id}
-              title={ticket.title}
-              description={ticket.description}
-              priority={ticket.priority}
-              assignedBy={ticket.author.displayName}
-            />
-          ))}
-      </ContentGrid>
-    </Layout>
-  );
+            <ContentGrid>
+                {filteredTickets.length > 0 &&
+                    filteredTickets.map((ticket) => (
+                        <TicketCard
+                            key={ticket.id}
+                            id={ticket.id}
+                            title={ticket.title}
+                            description={ticket.description}
+                            priority={ticket.priority}
+                            assignedBy={ticket.author.displayName}
+                        />
+                    ))}
+            </ContentGrid>
+        </Layout>
+    );
 };
 
 export default Tickets;
