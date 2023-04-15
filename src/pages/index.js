23563d4853af693a149f74ac11a28abfb7c1fecd
Mostafa Ezzat <psmostafa@gmail.com>
2023-04-15 11:40:41 +0200

    test

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,27 @@
 import { useState } from "react";
 
 // Components
-import SignUp from "../components/form/SignUp";
 import Signin from "../components/form/SignIn";
+import SignUp from "../components/form/SignUp";
 
 // Auth Check Components
 import NotAuthorized from "../components/auth/NotAuthorized";
 
 export default function Home() {
-  const [form, setForm] = useState("signup");
+    const [form, setForm] = useState("signup");
 
-  return (
-    <NotAuthorized>
-      <>
-        <div className="flex h-screen w-full flex-col items-center justify-center">
-          {form == "signup" ? (
-            <SignUp changeFrom={setForm} />
-          ) : (
-            <Signin changeFrom={setForm} />
-          )}
-        </div>
-      </>
-    </NotAuthorized>
-  );
+    return (
+        <NotAuthorized>
+            <>
+                welcome
+                <div className="flex h-screen w-full flex-col items-center justify-center">
+                    {form == "signup" ? (
+                        <SignUp changeFrom={setForm} />
+                    ) : (
+                        <Signin changeFrom={setForm} />
+                    )}
+                </div>
+            </>
+        </NotAuthorized>
+    );
 }
